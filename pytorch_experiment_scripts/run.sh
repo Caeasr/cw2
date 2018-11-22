1dfc78403853102eb6ebd9719a6d4aaa78afa5fa
Caesar <934042048@qq.com>
2018-11-22 19:39:23 +0000

    128

--- pytorch_experiment_scripts/run.sh
+++ pytorch_experiment_scripts/run.sh
@@ -1,10 +1,21 @@
 #!/bin/bash
 for filter in 4 16 32 64
 do
-        for layer in 1
+        for layer in 5
         do
                 name="mp_l"$layer"_f"$filter
                 python train_evaluate_emnist_classification_system.py --dim_reduction_type max_pooling --num_layers $layer --num_filters $filter --experiment_name $name --use_gpu True
       	done
 done
+
+
+for filter in 128
+do
+        for layer in 1 2 3 4 5
+        do
+                name="mp_l"$layer"_f"$filter
+                python train_evaluate_emnist_classification_system.py --dim_reduction_type max_pooling --num_layers $layer --num_filters $filter --experiment_name $name --use_gpu True
+        done
+done
+
 
